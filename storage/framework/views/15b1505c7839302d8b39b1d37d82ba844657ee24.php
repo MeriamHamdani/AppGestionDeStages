22e61a96e2a679c8d6fd424260f062e4bdc69f35
MeriamHamdani <mariem.hamdani@enis.tn>
2022-03-25 16:36:08 +0100

    framework views 2

---
+++ storage/framework/views/15b1505c7839302d8b39b1d37d82ba844657ee24.php
@@ -0,0 +1,117 @@
+
+
+<?php $__env->startSection('title'); ?>Liste des stages payés
+<?php echo e($title); ?>
+
+<?php $__env->stopSection(); ?>
+
+<?php $__env->startPush('css'); ?>
+<link rel="stylesheet" type="text/css" href="<?php echo e(asset('assets/css/datatables.css')); ?>">
+<link rel="stylesheet" type="text/css" href="<?php echo e(asset('assets/css/datatable-extension.css')); ?>">
+<?php $__env->stopPush(); ?>
+
+<?php $__env->startSection('content'); ?>
+<?php $__env->startComponent('components.breadcrumb'); ?>
+<?php $__env->slot('breadcrumb_title'); ?>
+<h3>La liste des stages payés</h3>
+<?php $__env->endSlot(); ?>
+<li class="breadcrumb-item">Paiement</li>
+<li class="breadcrumb-item">La liste des stages payés</li>
+
+<?php echo $__env->renderComponent(); ?>
+
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-sm-12">
+            <div class="card">
+                <div class="card-header pb-0">
+                    <h5>Mes stages payés</h5>
+                </div>
+                <div class="card-body">
+                    <div class="dt-ext table-responsive">
+                        <table class="display" id="auto-fill">
+                            <thead>
+                                <tr>
+
+                                    <th>Mon grade</th>
+                                    <th>Niveau de stagiaire</th>
+                                    <th>Mes frais (DT)</th>
+
+                                </tr>
+                            </thead>
+                            <tbody>
+
+                                <tr>
+
+                                    <td>Professeur</td>
+                                    <td>Mastère</td>
+                                    <td>500 </td>
+                                </tr>
+                                <tr>
+
+                                    <td>Maitre assistant</td>
+                                    <td>Ingénieurie</td>
+                                    <td>500 </td>
+                                </tr>
+                                <tr>
+
+                                    <td>Professeur</td>
+                                    <td>Licence</td>
+                                    <td>500 </td>
+
+                                </tr>
+
+                                <!-- <td style=" padding: 10px;
+                                    border: 2px solid #3CB371;
+                                    border-radius: 5px;
+                                    background-color: #e5e5e5;">En cours</td>-->
+                                </tr>
+
+                            </tbody>
+                            <tfoot>
+
+                                <tr>
+
+                                    <th>Mon grade</th>
+                                    <th>Niveau de stagiaire</th>
+                                    <th>Mes frais (DT)</th>
+
+                                </tr>
+
+                            </tfoot>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+<?php $__env->startPush('scripts'); ?>
+<script src="<?php echo e(asset('assets/js/datatable/datatables/jquery.dataTables.min.js')); ?>"></script>
+<script src="<?php echo e(asset('assets/js/datatable/datatable-extension/dataTables.buttons.min.js')); ?>"></script>
+<script src="<?php echo e(asset('assets/js/datatable/datatable-extension/jszip.min.js')); ?>"></script>
+<script src="<?php echo e(asset('assets/js/datatable/datatable-extension/buttons.colVis.min.js')); ?>"></script>
+<script src="<?php echo e(asset('assets/js/datatable/datatable-extension/pdfmake.min.js')); ?>"></script>
+<script src="<?php echo e(asset('assets/js/datatable/datatable-extension/vfs_fonts.js')); ?>"></script>
+<script src="<?php echo e(asset('assets/js/datatable/datatable-extension/dataTables.autoFill.min.js')); ?>"></script>
+<script src="<?php echo e(asset('assets/js/datatable/datatable-extension/dataTables.select.min.js')); ?>"></script>
+<script src="<?php echo e(asset('assets/js/datatable/datatable-extension/buttons.bootstrap4.min.js')); ?>"></script>
+<script src="<?php echo e(asset('assets/js/datatable/datatable-extension/buttons.html5.min.js')); ?>"></script>
+<script src="<?php echo e(asset('assets/js/datatable/datatable-extension/buttons.print.min.js')); ?>"></script>
+<script src="<?php echo e(asset('assets/js/datatable/datatable-extension/dataTables.bootstrap4.min.js')); ?>"></script>
+<script src="<?php echo e(asset('assets/js/datatable/datatable-extension/dataTables.responsive.min.js')); ?>"></script>
+<script src="<?php echo e(asset('assets/js/datatable/datatable-extension/responsive.bootstrap4.min.js')); ?>"></script>
+<script src="<?php echo e(asset('assets/js/datatable/datatable-extension/dataTables.keyTable.min.js')); ?>"></script>
+<script src="<?php echo e(asset('assets/js/datatable/datatable-extension/dataTables.colReorder.min.js')); ?>"></script>
+<script src="<?php echo e(asset('assets/js/datatable/datatable-extension/dataTables.fixedHeader.min.js')); ?>"></script>
+<script src="<?php echo e(asset('assets/js/datatable/datatable-extension/dataTables.rowReorder.min.js')); ?>"></script>
+<script src="<?php echo e(asset('assets/js/datatable/datatable-extension/dataTables.scroller.min.js')); ?>"></script>
+<script src="<?php echo e(asset('assets/js/datatable/datatable-extension/custom.js')); ?>"></script>
+<?php $__env->stopPush(); ?>
+
+<?php $__env->stopSection(); ?>
+
+
+<?php echo $__env->make('layouts.enseignant.master', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\Users\Lenovo\gestionDesStages\resources\views/enseignant/paiement/liste_stages_paye.blade.php ENDPATH**/ ?>
