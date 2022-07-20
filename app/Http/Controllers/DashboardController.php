9a1abfc4263aeb1e97fd58fbe4aa8a72f3561ac6
MeriamHamdani <mariem.hamdani@enis.tn>
2022-07-20 19:40:36 +0100

    pie chart pour stats des stages volontaires

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\AnneeUniversitaire;
 use App\Models\Classe;
 use App\Models\Departement;
 use App\Models\Enseignant;
@@ -11,8 +12,10 @@ use App\Models\Specialite;
 use App\Models\Stage;
 use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Http\Request;
+use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Str;
+use phpDocumentor\Reflection\PseudoTypes\List_;
 
 class DashboardController extends Controller
 {
@@ -29,8 +32,9 @@ class DashboardController extends Controller
         $stages = (StageController::stages1ereLicMaster())->count(); //dd($stages);
         $data = DashboardController::encadrementChart(); //dd($ids);
         $dataset = DashboardController::typeSujetChart(); //dd($dataset);
-        $ic = 5;
-        return view('admin.dashboard.dashboard', compact('nbre_etudiants', 'nbre_enseignants', 'nbre_departements', 'nbre_specialites', 'nbre_classes', 'nbre_entreprises', 'nbre_stages', 'data','ic','dataset'));
+        $statVol = DashboardController::stagesVolontairesChart(); //dd($statVol);
+        return view('admin.dashboard.dashboard', compact('nbre_etudiants', 'nbre_enseignants',
+            'nbre_departements', 'nbre_specialites', 'nbre_classes', 'nbre_entreprises', 'nbre_stages', 'data', 'dataset', 'statVol'));
     }
 
     public function encadrementChart()
@@ -50,22 +54,64 @@ class DashboardController extends Controller
             $c2++;
         }*/
         $c1 = null;
-        foreach($ids1 as $i) {
+        foreach ($ids1 as $i) {
             $c1++;
         }
         $data = [$c1, $idNonEncadrants];
         return $data;
     }
+
     public function typeSujetChart()
     {
         $stagesWithTypesSujets = Stage::select(DB::raw('type_sujet,count(*) as Nombre_des_stages'))->whereNotNull('type_sujet')->groupBy('type_sujet')->get()->toArray();
         $dataset = "";//dd($stagesWithTypesSujets);
-        foreach($stagesWithTypesSujets as $st) {
-            $dataset.= "['".array_values($st)[0]."',   ".
-                array_values($st)[1]."],";
+        foreach ($stagesWithTypesSujets as $st) {
+            $dataset .= "['" . array_values($st)[0] . "',   " .
+                array_values($st)[1] . "],";
         }
-        $array['dataset'] = rtrim($dataset,",");//dd($array);
+        $array['dataset'] = rtrim($dataset, ",");//dd($array);
         return $dataset;
+    }
+
+    public function stagesVolontairesChart()
+    {
+        $an = StageController::current_annee_univ();
+        $etudiants = Etudiant::with('user', 'classe')->where('annee_universitaire_id', $an->id)->get(); //dd($etudiants);
+        $classes = new Collection();
+        foreach ($etudiants as $etd) {
+            $type = Arr::last((TypeStageController::decouper_nom($etd->classe->typeStage->nom))); //dd($type);
+            if ($type == "Volontaire") {
+                $classes->push($etd->classe->nom); //dd($classes);
+            }
+        }
+        $cls = $classes->flatten()->unique()->toArray();
+        $etd1ereAnne = new Collection();
+        foreach ($etudiants as $etd) {
+            foreach ($cls as $c) {
+                if ($etd->classe->nom == $c) {
+                    $etd1ereAnne->push($etd);
+                }
+            }
+        }
+        $nbreEtdClsStgVolantaires = $etd1ereAnne->count();
+        $an = StageController::current_annee_univ();
+        $stagesVolontaires = StageController::stages1ereLicMaster()->where('annee_universitaire_id', $an->id);
+        $idEtdStages = new Collection();
+        $idEtdNonStages = new Collection();
+        $idTousEtds = new Collection();
+        foreach ($stagesVolontaires as $stg) {
+            $idEtdStages->push($stg->etudiant_id);
+        }
+        foreach ($etudiants as $etd) {
+            $idTousEtds->push($etd->id);
+        }
+        $idEtdNonStages = array_diff($idTousEtds->flatten()->unique()->toArray(), $idEtdStages->toArray());
+        $nbreEtdStgVolantaires = $idEtdStages->flatten()->unique()->count();// dd($idEtdNonStages,$idEtdStages);//dd($nbreEtdStgVolantaires,$idEtdStages);
+        $nbreEtdNonStgVolantaires = count(array_unique($idEtdNonStages)); //dd($nbreEtdNonStgVolantaires);
+       // dd($nbreEtdNonStgVolantaires);
+        $statVol = [$nbreEtdStgVolantaires, $nbreEtdNonStgVolantaires]; //dd($statVol);
+        return $statVol;
+
     }
 
 }
